96b21ef1521d230618c0a7dd79e62d3f16ba586a
James <wjamesfl@gmail.com>
2022-04-15 01:17:37 -0400

    nonbreaking change, add LogTypes

--- editor/src/main/com/mbrlabs/mundus/editor/events/LogEvent.kt
+++ editor/src/main/com/mbrlabs/mundus/editor/events/LogEvent.kt
@@ -1,9 +1,17 @@
 package com.mbrlabs.mundus.editor.events
 
+enum class LogType {
+    INFO,
+    ERROR
+}
+
 /**
  * An Event for posting new log entries in the log bar
  */
-class LogEvent(val logMessage: String) {
+class LogEvent(val logType: LogType = LogType.INFO, val logMessage: String) {
+
+    // Secondary constructor (to support Java classes) to use INFO as default
+    constructor(logMessage: String) : this(LogType.INFO, logMessage)
 
     interface LogEventListener {
         @Subscribe
