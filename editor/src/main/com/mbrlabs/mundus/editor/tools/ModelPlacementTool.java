0d776112bcd36e634eddff9b9f1c9b17bc83db1b
James <wjamesfl@gmail.com>
2022-05-24 16:22:40 -0400

    Fix NPE on model placement

--- editor/src/main/com/mbrlabs/mundus/editor/tools/ModelPlacementTool.java
+++ editor/src/main/com/mbrlabs/mundus/editor/tools/ModelPlacementTool.java
@@ -118,6 +118,7 @@ public class ModelPlacementTool extends Tool {
 
             PickableModelComponent modelComponent = new PickableModelComponent(modelGo, Shaders.INSTANCE.getModelShader());
             modelComponent.setShader(getShader());
+            modelComponent.setDepthShader(Shaders.INSTANCE.getDepthShader());
             modelComponent.setModel(model, true);
             modelComponent.encodeRaypickColorId();
 
