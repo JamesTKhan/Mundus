14bf19de4ed1a025d4a9e1d4005cc470c2d80cba
James <wjamesfl@gmail.com>
2022-07-01 01:15:05 -0400

    Figuring out lights for PBR

--- commons/src/main/com/mbrlabs/mundus/commons/shaders/MundusPBRShader.java
+++ commons/src/main/com/mbrlabs/mundus/commons/shaders/MundusPBRShader.java
@@ -1,6 +1,10 @@
 package com.mbrlabs.mundus.commons.shaders;
 
+import com.badlogic.gdx.graphics.g3d.Attributes;
 import com.badlogic.gdx.graphics.g3d.Renderable;
+import com.badlogic.gdx.utils.Array;
+import com.mbrlabs.mundus.commons.env.lights.DirectionalLight;
+import com.mbrlabs.mundus.commons.env.lights.DirectionalLightsAttribute;
 import net.mgsx.gltf.scene3d.shaders.PBRShader;
 
 /**
@@ -11,5 +15,23 @@ public class MundusPBRShader extends PBRShader {
 
     public MundusPBRShader(Renderable renderable, Config config, String prefix) {
         super(renderable, config, prefix);
+    }
+
+    @Override
+    protected void bindLights(Renderable renderable, Attributes attributes) {
+        //super.bindLights(renderable, attributes);
+
+        // directional lights
+        final DirectionalLightsAttribute dirLightAttribs = renderable.environment.get(DirectionalLightsAttribute.class,
+                DirectionalLightsAttribute.Type);
+        final Array<DirectionalLight> dirLights = dirLightAttribs == null ? null : dirLightAttribs.lights;
+        if (dirLights != null && dirLights.size > 0) {
+            final DirectionalLight light = dirLights.first();
+            int idx = dirLightsLoc + 0 * dirLightsSize;
+            program.setUniformf(idx + dirLightsColorOffset, light.color.r * light.intensity, light.color.g * light.intensity,
+                    light.color.b * light.intensity);
+            program.setUniformf(idx + dirLightsDirectionOffset, light.direction.x,
+                    light.direction.y, light.direction.z);
+        }
     }
 }
