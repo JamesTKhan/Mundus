3745dd477e4c14431ad02fd4fe44ffd5b962a8b1
James <wjamesfl@gmail.com>
2022-06-30 20:13:01 -0400

    missing import

--- commons/src/main/com/mbrlabs/mundus/commons/Scene.java
+++ commons/src/main/com/mbrlabs/mundus/commons/Scene.java
@@ -17,6 +17,7 @@
 package com.mbrlabs.mundus.commons;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.Pixmap;
