422d11d0a5883128723889176bbc176d6430e0a4
Zsuró Tibor <zsurotibor@gmail.com>
2023-05-01 20:00:51 +0000

    Game object can store custom value and widget can load them

--- editor/src/main/com/mbrlabs/mundus/editor/ui/modules/inspector/components/CustomPropertiesWidget.kt
+++ editor/src/main/com/mbrlabs/mundus/editor/ui/modules/inspector/components/CustomPropertiesWidget.kt
@@ -1,16 +1,21 @@
 package com.mbrlabs.mundus.editor.ui.modules.inspector.components
 
+import com.badlogic.gdx.scenes.scene2d.Actor
 import com.badlogic.gdx.scenes.scene2d.InputEvent
+import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener
 import com.kotcrab.vis.ui.widget.VisTable
 import com.kotcrab.vis.ui.widget.VisTextButton
 import com.kotcrab.vis.ui.widget.VisTextField
 import com.mbrlabs.mundus.commons.scene3d.GameObject
 import com.mbrlabs.mundus.editor.ui.modules.inspector.BaseInspectorWidget
+import com.mbrlabs.mundus.editor.ui.widgets.FaTextButton
+import com.mbrlabs.mundus.editor.utils.Fa
 
 class CustomPropertiesWidget : BaseInspectorWidget("Custom Properties") {
 
     private val customProperties = VisTable()
+    private var gameObject: GameObject? = null
 
     init {
         isDeletable = false
@@ -22,9 +27,14 @@ class CustomPropertiesWidget : BaseInspectorWidget("Custom Properties") {
     }
 
     override fun setValues(go: GameObject) {
+        this.gameObject = go
+        val goCustomProperties = go.customProperties
+
         customProperties.clearChildren()
 
-        // TODO add custom properties
+        for (entry in goCustomProperties) {
+            addCustomProperty(entry.key, entry.value)
+        }
     }
 
     private fun setupUI() {
@@ -35,8 +45,46 @@ class CustomPropertiesWidget : BaseInspectorWidget("Custom Properties") {
 
         addButton.addListener(object : ClickListener() {
             override fun clicked(event: InputEvent?, x: Float, y: Float) {
-                customProperties.add(VisTextField()).padBottom(3f).padRight(3f)
-                customProperties.add(VisTextField()).padBottom(3f).row()
+                val key = ""
+                val value = ""
+                gameObject!!.customProperties.put(key, value)
+                addCustomProperty(key, value)
+            }
+        })
+    }
+
+    private fun addCustomProperty(key: String, value: String) {
+        var previousKey = key
+
+        val keyTextField = VisTextField(key)
+        val valueTextField = VisTextField(value)
+        val deleteButton = FaTextButton(Fa.TIMES)
+
+        customProperties.add(keyTextField).padBottom(3f).padRight(3f)
+        customProperties.add(valueTextField).padBottom(3f).padRight(3f)
+        customProperties.add(deleteButton).row()
+
+        keyTextField.addListener(object : ChangeListener() {
+            override fun changed(event: ChangeEvent, actor: Actor) {
+                val currentKey = keyTextField.text
+
+                val goCustomProperties = gameObject!!.customProperties
+
+                goCustomProperties.remove(previousKey)
+                goCustomProperties.put(currentKey, valueTextField.text)
+
+                previousKey = currentKey
+            }
+        })
+
+        valueTextField.addListener(object : ChangeListener() {
+            override fun changed(event: ChangeEvent?, actor: Actor?) {
+                val currentKey = keyTextField.text
+                val currentValue = valueTextField.text
+
+                val goCustomProperties = gameObject!!.customProperties
+
+                goCustomProperties.put(currentKey, currentValue)
             }
         })
     }
