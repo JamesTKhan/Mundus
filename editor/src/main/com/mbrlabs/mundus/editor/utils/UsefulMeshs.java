a2acee39256155b8be37d1ac8c594c70e321c0cc
James <wjamesfl@gmail.com>
2023-06-19 14:41:49 -0400

    ability to add primitive meshes

--- editor/src/main/com/mbrlabs/mundus/editor/utils/UsefulMeshs.java
+++ editor/src/main/com/mbrlabs/mundus/editor/utils/UsefulMeshs.java
@@ -17,6 +17,8 @@ package com.mbrlabs.mundus.editor.utils;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Mesh;
+import com.badlogic.gdx.graphics.VertexAttribute;
 import com.badlogic.gdx.graphics.VertexAttributes;
 import com.badlogic.gdx.graphics.VertexAttributes.Usage;
 import com.badlogic.gdx.graphics.g3d.Material;
@@ -126,6 +128,148 @@ public class UsefulMeshs {
             }
         }
 
+        return modelBuilder.end();
+    }
+
+    public static Model createPlane(Material mat, float size) {
+        // Normal, Position, TextureCoordinates
+        float[] vertices = {
+                0.0f, 1.0f, -0.0f,
+                -size, 0.0f, size,
+                0.0f, 1.0f,
+
+                0.0f, 1.0f, -0.0f,
+                size, 0.0f, size,
+                1.0f, 1.0f,
+
+                0.0f, 1.0f, -0.0f,
+                -size, 0.0f, -size,
+                0.0f, 0.0f,
+
+                0.0f, 1.0f, -0.0f,
+                size, 0.0f, -size,
+                1.0f, 0.0f
+        };
+        short[] indices = {0, 1, 3, 0, 3, 2};
+
+        Mesh mesh = new Mesh(false, vertices.length, indices.length, VertexAttribute.Normal(), VertexAttribute.Position(), VertexAttribute.TexCoords(0));
+        mesh.setVertices(vertices);
+        mesh.setIndices(indices);
+
+        ModelBuilder modelBuilder = new ModelBuilder();
+        modelBuilder.begin();
+        modelBuilder.part("plane", mesh, GL20.GL_TRIANGLES, mat);
+        return modelBuilder.end();
+    }
+
+    public static Model createCube(Material mat, float size) {
+        // Normal, Position, TextureCoordinates
+        float[] vertices = {
+                0.0f, 0.0f, -1.0f,
+                size, size, -size,
+                0.625f, 0.5f,
+
+                0.0f, 1.0f, -0.0f,
+                size, size, -size,
+                0.625f, 0.5f,
+
+                1.0f, 0.0f, -0.0f,
+                size, size, -size,
+                0.625f, 0.5f,
+
+                0.0f, -1.0f, -0.0f,
+                size, -size, -size,
+                0.375f, 0.5f,
+
+                0.0f, 0.0f, -1.0f,
+                size, -size, -size,
+                0.375f, 0.5f,
+
+                1.0f, 0.0f, -0.0f,
+                size, -size, -size,
+                0.375f, 0.5f,
+
+                0.0f, 0.0f, 1.0f,
+                size, size, size,
+                0.625f, 0.25f,
+
+                0.0f, 1.0f, -0.0f,
+                size, size, size,
+                0.625f, 0.25f,
+
+                1.0f, 0.0f, -0.0f,
+                size, size, size,
+                0.625f, 0.25f,
+
+                0.0f, -1.0f, -0.0f,
+                size, -size, size,
+                0.375f, 0.25f,
+
+                0.0f, 0.0f, 1.0f,
+                size, -size, size,
+                0.375f, 0.25f,
+
+                1.0f, 0.0f, -0.0f,
+                size, -size, size,
+                0.375f, 0.25f,
+
+                -1.0f, 0.0f, -0.0f,
+                -size, size, -size,
+                0.625f, 0.75f,
+
+                0.0f, 0.0f, -1.0f,
+                -size, size, -size,
+                0.625f, 0.75f,
+
+                0.0f, 1.0f, -0.0f,
+                -size, size, -size,
+                0.875f, 0.5f,
+
+                -1.0f, 0.0f, -0.0f,
+                -size, -size, -size,
+                0.375f, 0.75f,
+
+                0.0f, -1.0f, -0.0f,
+                -size, -size, -size,
+                0.125f, 0.5f,
+
+                0.0f, 0.0f, -1.0f,
+                -size, -size, -size,
+                0.375f, 0.75f,
+
+                -1.0f, 0.0f, -0.0f,
+                -size, size, size,
+                0.625f, 1.0f,
+
+                0.0f, 0.0f, 1.0f,
+                -size, size, size,
+                0.625f, 0.0f,
+
+                0.0f, 1.0f, -0.0f,
+                -size, size, size,
+                0.875f, 0.25f,
+
+                -1.0f, 0.0f, -0.0f,
+                -size, -size, size,
+                0.375f, 1.0f,
+
+                0.0f, -1.0f, -0.0f,
+                -size, -size, size,
+                0.125f, 0.25f,
+
+                0.0f, 0.0f, 1.0f,
+                -size, -size, size,
+                0.375f, 0.0f
+        };
+        short[] indices = {1, 14, 20, 1, 20, 7, 10, 6, 19, 10, 19, 23, 21, 18, 12, 21, 12, 15, 16, 3, 9, 16, 9, 22, 5, 2, 8, 5, 8, 11, 17, 13, 0, 17, 0, 4};
+
+        Mesh mesh = new Mesh(false, vertices.length, indices.length, VertexAttribute.Normal(), VertexAttribute.Position(), VertexAttribute.TexCoords(0));
+        mesh.setVertices(vertices);
+        mesh.setIndices(indices);
+
+        ModelBuilder modelBuilder = new ModelBuilder();
+        modelBuilder.begin();
+        modelBuilder.part("cube", mesh, GL20.GL_TRIANGLES, mat);
         return modelBuilder.end();
     }
 
