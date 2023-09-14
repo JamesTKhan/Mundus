28f1d91d69302d22920f2e3e2eb986b1488c51fe
Todd <toddsidoti@gmail.com>
2023-09-14 15:10:23 -0800

    good to go but requires project reload, update to most recent terrain_chunks

--- commons/src/main/com/mbrlabs/mundus/commons/scene3d/components/TerrainComponent.java
+++ commons/src/main/com/mbrlabs/mundus/commons/scene3d/components/TerrainComponent.java
@@ -253,7 +253,7 @@ public class TerrainComponent extends CullableComponent implements AssetUsage, R
      * @return The out Vector3 which contains the intersect point.
      */
     public Vector3 getRayIntersection(Vector3 out, Ray ray) {
-        terrainAsset.getTerrain().getRayIntersection(out, ray, modelInstance.transform);
+        terrainAsset.getTerrain().getRayIntersection(out, ray, currentInstance.transform);
         return out;
     }
 
