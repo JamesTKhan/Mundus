0fee9cb02dd45375fad8aa31449416b8bbfb159a
Zsuró Tibor <zsurotibor@gmail.com>
2023-11-26 21:26:32 +0000

    Add radio buttons

--- editor/src/main/com/mbrlabs/mundus/editor/ui/modules/inspector/components/terrain/TerrainObjectTab.kt
+++ editor/src/main/com/mbrlabs/mundus/editor/ui/modules/inspector/components/terrain/TerrainObjectTab.kt
@@ -17,20 +17,46 @@
 package com.mbrlabs.mundus.editor.ui.modules.inspector.components.terrain
 
 import com.badlogic.gdx.scenes.scene2d.ui.Table
+import com.badlogic.gdx.utils.Align
+import com.kotcrab.vis.ui.widget.VisLabel
+import com.kotcrab.vis.ui.widget.VisRadioButton
 import com.kotcrab.vis.ui.widget.VisTable
 import com.mbrlabs.mundus.editor.tools.brushes.TerrainBrush
+import com.mbrlabs.mundus.editor.ui.widgets.ImprovedSlider
 import com.mbrlabs.mundus.editor.ui.widgets.TerrainObjectLayerWidget
 
 class TerrainObjectTab(private val parentWidget: TerrainComponentWidget) : BaseBrushTab(parentWidget, TerrainBrush.BrushMode.TERRAIN_OBJECT) {
 
     private val root = VisTable()
     private val terrainObjectLayerWidget = TerrainObjectLayerWidget()
+    private val actionWidget = VisTable()
+    private val addingActionWidget = VisTable()
 
     init {
         root.add(terrainObjectLayerWidget).expand().fill().padBottom(5f).row()
+
+        val radioButtonTable = VisTable()
+        radioButtonTable.add(VisRadioButton("Adding")).expand().align(Align.center)
+        radioButtonTable.add(VisRadioButton("Removing")).expand().align(Align.center)
+        root.add(radioButtonTable).expandX().fillX().row()
+
+        actionWidget.add(addingActionWidget).expandX().fillX()
+
+        root.add(actionWidget).expandX().fillX()
+
+        setupAddingActionWidget()
+
+//        root.setDebug(true, true) // TODO remove later
     }
 
     override fun getTabTitle(): String = "Objets"
 
     override fun getContentTable(): Table = root
+
+    private fun setupAddingActionWidget() {
+        val strengthSlider = ImprovedSlider(0f, 1f, 0.1f)
+        addingActionWidget.add(VisLabel("Strength")).left().row()
+        strengthSlider.value = TerrainBrush.getStrength() // TODO
+        addingActionWidget.add(strengthSlider).expandX().fillX().row()
+    }
 }
