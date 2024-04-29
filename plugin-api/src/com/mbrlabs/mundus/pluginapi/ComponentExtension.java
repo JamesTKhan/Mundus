a30481c762d4ecda26d4d9a818b82dfe3858856a
Zsuró Tibor <zsurotibor@gmail.com>
2024-04-29 14:44:05 +0000

    Add setupComponentInspectorWidget into ComponentExtension

--- plugin-api/src/com/mbrlabs/mundus/pluginapi/ComponentExtension.java
+++ plugin-api/src/com/mbrlabs/mundus/pluginapi/ComponentExtension.java
@@ -2,6 +2,7 @@ package com.mbrlabs.mundus.pluginapi;
 
 import com.mbrlabs.mundus.commons.scene3d.GameObject;
 import com.mbrlabs.mundus.commons.scene3d.components.Component;
+import com.mbrlabs.mundus.pluginapi.ui.RootWidget;
 import org.pf4j.ExtensionPoint;
 
 public interface ComponentExtension extends ExtensionPoint {
@@ -9,4 +10,6 @@ public interface ComponentExtension extends ExtensionPoint {
     String getComponentName();
 
     Component createComponent(GameObject gameObject);
+
+    void setupComponentInspectorWidget(RootWidget rootWidget);
 }
